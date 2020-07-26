2946ae92e139acd3df26b1ad2f18dc2237ef222d
Thomas <thomas.laine25@gmail.com>
2020-07-26 20:06:51 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -29,4 +29,3 @@ export default class App extends React.Component {
       );
   }
 }
-
--- src/components/searcharticle.js
+++ src/components/searcharticle.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const InputDiv = styled.div `
     text-align: center;
     margin-top: 1em;
-`
+`;
+
+const StyledAnchor = styled.a `
+    text-decoration: none !important;
+`;
+
+const ArticleDiv = styled.div `
+    color: white;
+    text-align: left;
+    background-color: grey;
+    border: 1px solid white;
+    margin-bottom: 1em;
+`;
 
 const StyledParagraph = styled.p `
     color: white;
     text-align: center;
     margin-top: 1em;
-`
-
-const StyledIcon = styled.span `
-    color: orange;
-    font-size: 3em;
-    cursor: pointer;
 `;
 
-const StyledInput = styled.input`
-    display: none;
+const StyledInput = styled.input.attrs({
+    type: "text"
+})`
     margin: auto;
     border: 4px solid orange;
     background-color: transparent;
@@ -38,33 +43,55 @@ const StyledInput = styled.input`
 export default class SearchArticle extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            displayIcon: "inline",
-            displayInput: "none"
+            inputVal: "",
+            articles: []
         }
 
-        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleClick() {
+    handleChange = (event) => {
+        let targetEvent = event.target.value;
         this.setState({
-            displayIcon: "none",
-            displayInput: "block"
+            inputVal: targetEvent
         })
     }
 
+    handleSubmit = (event) => {
+        let searchExp = this.state.inputVal;
+        let req = 'https://en.wikipedia.org/api/rest_v1/page/summary/'+ searchExp;
+        
+        axios.get(req)
+        .then(res => {
+          const article = res.data;
+         this.setState({ articles: [...this.state.articles, article] }, () => {console.log(this.state.articles)});
+        })
+    
+        event.preventDefault();
+    }
+
     render() {
+        let article = this.state.articles;
         return (
-            <InputDiv>
-                <StyledIcon>
-                    <FontAwesomeIcon 
-                        icon={faSearch} 
-                        onClick={this.handleClick}
-                        style={{display: this.state.displayIcon}} />
-                </StyledIcon>
-                <StyledInput type="text" style={{display: this.state.displayInput}} />
-                <StyledParagraph>Click icon to search</StyledParagraph>
-            </InputDiv>
+            <React.Fragment>
+                <InputDiv>
+                    <form onSubmit={this.handleSubmit}>
+                        <StyledInput onChange={this.handleChange} value={this.state.inputVal} />
+                    </form>
+                    <StyledParagraph>Type something to search</StyledParagraph>
+                </InputDiv>
+                {article.map((item) => 
+                    <StyledAnchor href={item.content_urls.desktop.page} target="_blank" rel="noopener noreferrer">
+                        <ArticleDiv>
+                            <h1>{item.title}</h1>
+                            <p>{item.extract}</p>
+                    </ArticleDiv>
+                    </StyledAnchor>
+                )} 
+            </React.Fragment>   
         );
     }
 }
