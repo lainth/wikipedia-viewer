4bded98131c2322c475c72ed09701b9d12480c29
Thomas <thomas.laine25@gmail.com>
2020-07-26 20:13:11 +0200

    Updates

---
+++ precache-manifest.6513ae0f91693ed32ac0d388fd753d2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d1588c926838a2c010916cf0b61a6b",
+    "url": "/wikipedia-viewer/index.html"
+  },
+  {
+    "revision": "13aa01825d602f32b818",
+    "url": "/wikipedia-viewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e36162a0098affb2674f",
+    "url": "/wikipedia-viewer/static/css/main.a7c03c1a.chunk.css"
+  },
+  {
+    "revision": "13aa01825d602f32b818",
+    "url": "/wikipedia-viewer/static/js/2.a23981da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wikipedia-viewer/static/js/2.a23981da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36162a0098affb2674f",
+    "url": "/wikipedia-viewer/static/js/main.3d54bbc2.chunk.js"
+  },
+  {
+    "revision": "fc76de0ac4f9ff276e79",
+    "url": "/wikipedia-viewer/static/js/runtime-main.ae763e8e.js"
+  }
+]);
